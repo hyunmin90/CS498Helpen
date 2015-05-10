fa9ca4f9eb0fb8662dbd534f78aefa12aae55485
Dong Min Shin <dshin9@illinois.edu>
2015-05-10 03:36:14 +0000

    added schemas

--- models/subject.js
+++ models/subject.js
@@ -2,11 +2,20 @@ var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
 
 var SubjectSchema = new Schema({
-    subjectCode: String,
-    subjectNumber: String,
-    instructor: String,
-    description: String
+    subjectCode: { 
+    	type: String,
+    	required: 'Please provide course code'
+    },
+    subjectNumber: {
+    	type: String,
+    	required: 'Please provide course number'
+    },
+    instructor: {
+    	type: String
+    },
+    description: {
+    	type: String
+    }
 });
 
-// Export the Mongoose model
 mongoose.model('Subject', SubjectSchema);
